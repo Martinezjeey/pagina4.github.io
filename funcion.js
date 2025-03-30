4c32df5778abffd0ba4dbe5a6f59bfdb75d5dec6
Martinezjeey <130194437+Martinezjeey@users.noreply.github.com>
2025-03-30 01:04:55 -0600

    Add files via upload

---
+++ funcion.js
@@ -0,0 +1,19 @@
+r.addEventListener("click", () => {
+    alert("El producto seleccionado actualmente no esta disponible")
+})
+
+s.addEventListener("click", () => {
+    alert("El producto seleccionado actualmente no esta disponible")
+})
+
+t.addEventListener("click", () => {
+    alert("El producto seleccionado actualmente no esta disponible")
+})
+
+x.addEventListener("click", () => {
+    alert("El producto seleccionado actualmente no esta disponible")
+})
+
+y.addEventListener("click", () => {
+    alert("El producto seleccionado actualmente no esta disponible")
+})
